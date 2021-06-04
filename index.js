0923d9fb4434348ddc622e477b0268a7c7e8899a
rajonahmed749 <raazon749@gmail.com>
2021-06-05 01:07:18 +0600

    heroku server hosting added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
 const port = process.env.PORT || 4000;
 
 const app = express();
@@ -22,7 +23,9 @@ app.get('/', (req, res) => {
 
 client.connect(err => {
   const bookCollection = client.db("theBookStore").collection("books");
+  const orderCollection = client.db("theBookStore").collection("orders");
 
+  // get all books
   app.get('/allBooks', (req, res) => {
     bookCollection.find({})
     .toArray((err, documents) =>{
@@ -30,16 +33,51 @@ client.connect(err => {
     })
   })
 
-
+  // add book one by one
   app.post('/addBook', (req, res) => {
-    const newBook = req.body;
-    console.log("new books", newBook);
+    const newBook = req.body;    
     bookCollection.insertOne(newBook)
     .then(result => {
-      console.log("insert one", result.insertedCount);
+      res.send(result.insertedCount > 0)
+    })
+  });
+
+
+  // checkout theBookStore
+    app.get("/book/:id", (req, res) => {    
+    bookCollection.find({_id: ObjectId(req.params.id)})
+    .toArray((err, documents) =>{
+      res.send(documents[0]);
+      // console.log("this is documents",documents);
+
+    })
+  })
+
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    orderCollection.insertOne(order)
+    .then(result =>{
       res.send(result.insertedCount > 0)
     })
   })
+  app.get('/allOrder', (req, res) => {
+    orderCollection.find({})
+    .toArray((err, documents) =>{
+      res.send(documents)
+    })
+  })
+
+
+  // delete book
+  app.delete("/delete/:id", (req, res) => {
+    bookCollection.deleteOne({ _id: ObjectId(req.params.id)})
+    .then(result => console.log(result.deletedCount > 0))
+  })
+
+
+
+
+
 });
 
 
