981c35321881a0ac883a4696262a94e63025092a
rajonahmed749 <raazon749@gmail.com>
2021-06-27 22:27:08 +0600

    server done

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors())
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqa8e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -20,7 +19,6 @@ app.get('/', (req, res) => {
   console.log("database connected")
 })
 
-
 client.connect(err => {
   const bookCollection = client.db("theBookStore").collection("books");
   const orderCollection = client.db("theBookStore").collection("orders");
@@ -28,69 +26,62 @@ client.connect(err => {
   // get all books
   app.get('/allBooks', (req, res) => {
     bookCollection.find({})
-    .toArray((err, documents) =>{
-      res.send(documents)
-    })
+      .toArray((err, documents) => {
+        res.send(documents)
+      })
   })
 
   // add book one by one
   app.post('/addBook', (req, res) => {
-    const newBook = req.body;    
+    const newBook = req.body;
     bookCollection.insertOne(newBook)
-    .then(result => {
-      res.send(result.insertedCount > 0)
-    })
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
   });
 
 
   // checkout theBookStore
-    app.get("/book/:id", (req, res) => {    
-    bookCollection.find({_id: ObjectId(req.params.id)})
-    .toArray((err, documents) =>{
-      res.send(documents[0]);
-      // console.log("this is documents",documents);
-
-    })
+  app.get("/book/:id", (req, res) => {
+    bookCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      })
   })
   // place an order
   app.post("/addOrder", (req, res) => {
     const order = req.body;
     orderCollection.insertOne(order)
-    .then(result =>{
-      res.send(result.insertedCount > 0 )
-    })
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
   })
 
   // get all orders
   app.get('/allOrder', (req, res) => {
     orderCollection.find({})
-    .toArray((err, documents) =>{
-      res.send(documents)
-    })
+      .toArray((err, documents) => {
+        res.send(documents)
+      })
   })
 
 
-    //get specific user data
-    app.get('/user-data', (req, res) => {
-      userCollection.find({ email: req.query.email })
-        .toArray((err, documents) => {
-          res.send(documents)
-        })
-    })
+  //get specific user data
+  app.get('/user-data', (req, res) => {
+    userCollection.find({ email: req.query.email })
+      .toArray((err, documents) => {
+        res.send(documents)
+      })
+  })
 
 
   // delete book
   app.delete("/delete/:id", (req, res) => {
-    bookCollection.deleteOne({ _id: ObjectId(req.params.id)})
-    .then(result => console.log(result.deletedCount > 0))
+    bookCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(result => console.log(result.deletedCount > 0))
   })
 
-
-
-
-
 });
-
 
 
 app.listen(process.env.PORT || port)
